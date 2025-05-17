df1cbe33ab1b78adfa0f5ed5af8d31e09695344f
Pulipull <syaifularifin076@gmail.com>
2025-05-18 00:27:04 +0700

    artikel

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,7 +10,7 @@
   <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-600">{{ $post ['title'] }}</h2>
   <div class="text-base text-gray-500">
   </a>
-    <a href="#">{{ $post ['author'] }}</a> | 2 February 2022
+    <a href="#">{{ $post ['author'] }}</a> | {{ $post->created_at->diffForHumans() }}
   </div>
   <p class="my-4 font-light">{{Str::limit ($post ['body'], 100) }}</p>
   <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-600 hover:underline">Read More &raquo; </a>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Post;
+use Faker\Provider\Lorem;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -14,46 +16,14 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title'=> 'Blog', 'posts' => [
-        [
-            'id' => '1',
-            'title' => 'Lorem1',
-            'author' => 'Syaiful Arifin',
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi quaerat ipsam sunt impedit provident id necessitatibus, tenetur facilis similique eos incidunt quae. Rerum, ipsa voluptas? Saepe quisquam repellat soluta.'
-        ],
-        [
-            'id' => '2',
-            'title' => 'Lorem2',
-            'author' => 'Syaiful Arifin',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis soluta explicabo accusamus nisi maxime omnis facilis veniam odio? Ad corrupti cumque placeat inventore soluta tenetur officiis nulla ipsa atque maiores?'
-        ]
-    ]]);
+    return view('posts', ['title'=> 'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/contact', function () {
     return view('contact', ['title'=> 'Contact']);
 });
 
-Route:: get('/posts/{id}', function($id){
-$posts = [
-        [
-            'id' => '1',
-            'slug'=> 'lorem-1',
-            'title' => 'Lorem1',
-            'author' => 'Syaiful Arifin',
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi quaerat ipsam sunt impedit provident id necessitatibus, tenetur facilis similique eos incidunt quae. Rerum, ipsa voluptas? Saepe quisquam repellat soluta.'
-        ],
-        [
-            'id' => '2',
-            'slug'=> 'lorem-2',
-            'title' => 'Lorem2',
-            'author' => 'Syaiful Arifin',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis soluta explicabo accusamus nisi maxime omnis facilis veniam odio? Ad corrupti cumque placeat inventore soluta tenetur officiis nulla ipsa atque maiores?'
-        ]
-        ];
-
-    $post = Arr::first($posts, function($post) use ($id){
-        return $post['id']==$id;
-    });
+Route:: get('/posts/{post}', function(Post $post){
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
+
