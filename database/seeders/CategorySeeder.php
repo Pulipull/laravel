a0fafb52930343fce8a1a0fa6bd2c01292e262a9
Pulipull <syaifularifin076@gmail.com>
2025-05-18 17:46:10 +0700

    seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Category::factory(3)->create();
+        Category::create([
+            'name'=>'UI-UX'
+        ]);
+        Category::create([
+            'name'=>'Machine Learning'
+        ]);
+        Category::create([
+            'name'=>'Web Design'
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use function Laravel\Prompts\password;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +21,27 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        
+
+        // Category::create([
+        //     'name' => 'Web Design'
+
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul artikel1',
+        //     'author_id' => '1',
+        //     'category_id' => '1',
+        //     'body' => 'Laravel includes the ability to seed your database with data using seed classes. All seed classes are stored in the database/seeders directory. By default, a DatabaseSeeder class is defined for you. From this class, you may use the call method to run other seed classes, allowing you to control the seeding order.'
+        // ]);
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+            ])->create();
     }
 }
